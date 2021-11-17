6e613a336d5b8de42733a1a237b0726a17c3ac5d
Michael Hans <michaelhans777@gmail.com>
2021-11-17 16:03:46 +0700

    Initial Repository and CSV

---
+++ src/Struk.py
@@ -0,0 +1,9 @@
+class Struk:
+    def __init__(self, id: str, date: str, total: int, pay: int, exchange: int):
+        self.id = id
+        self.date = date
+        self.total = total
+        self.pay = pay
+        self.exchange = exchange
+
+    
---
+++ src/database/main.py
@@ -0,0 +1,29 @@
+# Main program
+class Swalayan:
+    def __init__(self, path: str):
+        # Load the database
+        pass
+    
+    def create_struk(self):
+        pass
+
+    def insert_struk(self):
+        pass
+
+    def calculate_struk(self):
+        pass
+
+    def payment(self, nominal: int):
+        pass
+
+    def cancel_struk(self):
+        pass
+
+    def display_struk(self, start_date, end_date):
+        pass
+
+    def display_peak(self, start_date="", end_date=""):
+        pass
+
+    def best_product(self, start_date="", end_date=""):
+        pass
